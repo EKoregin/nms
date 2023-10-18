d3c980145c5a39e221cfe647f85e60157718da68
Evgeny Koregin <evgkoregin@gmail.com>
2023-10-18 18:28:12 +0300

    Add index.html Add devices.html Add addModelDevice.html Add modelDevices.html

--- src/main/java/com/ekoregin/nms/controller/ModelDeviceController.java
+++ src/main/java/com/ekoregin/nms/controller/ModelDeviceController.java
@@ -3,21 +3,30 @@ package com.ekoregin.nms.controller;
 import com.ekoregin.nms.dto.ModelDeviceDto;
 import com.ekoregin.nms.entity.ModelDevice;
 import com.ekoregin.nms.service.ModelDeviceService;
+import com.ekoregin.nms.util.TypeDevice;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.NoSuchElementException;
 
 @Slf4j
 @RequiredArgsConstructor
 @Controller
-@RequestMapping("/modelDevice")
+@RequestMapping("/modelDevices")
 public class ModelDeviceController {
 
     public final ModelDeviceService modelDeviceService;
+
+    @ModelAttribute
+    public void addDeviceType(Model model) {
+        List<String> types = Arrays.stream(TypeDevice.class.getEnumConstants()).map(Enum::name).toList();
+        model.addAttribute("deviceTypes", types);
+    }
 
     @GetMapping
     public String allModelDevices(Model model) {
