010521f8a724826bd5a0cfea83780b6278c04465
Evgeny Koregin <evgkoregin@gmail.com>
2023-10-26 15:08:29 +0300

    Add snmp4j dependency

--- src/main/java/com/ekoregin/nms/util/CheckExecutorSnmp.java
+++ src/main/java/com/ekoregin/nms/util/CheckExecutorSnmp.java
@@ -3,9 +3,15 @@ package com.ekoregin.nms.util;
 import com.ekoregin.nms.entity.*;
 import com.ekoregin.nms.service.ModelDeviceService;
 import io.hypersistence.utils.hibernate.type.basic.Inet;
-import lombok.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 @Getter
@@ -31,19 +37,33 @@ public class CheckExecutorSnmp implements CheckExecutor {
             throw new RuntimeException("Check device cannot be null!");
 
         // Получаем необходимые для проверки параметры и сохраняем их в мапу
-//        Map<String, String> paramsForCheck = new HashMap<>();
-//        for(TypeTechParameter type: check.getTypeTechParams()) {
-//
-//        }
+        Map<String, String> paramsForCheck = new HashMap<>();
+        for (TypeTechParameter type : check.getTypeTechParams()) {
+            String paramValue = customer.getParams().stream()
+                    .filter(parameter -> parameter.getType().equals(type))
+                    .map(TechParameter::getValue)
+                    .findFirst().orElse(Strings.EMPTY);
+            paramsForCheck.put(type.getName(), paramValue);
+        }
 
+        log.info("Params and values: {}", paramsForCheck);
+        // Необходимо в конечном OID заменить пераметры типа {PORT} на значения, указанные у пользователя
+        String snmpOID = updateVariablesInOIDwithValues(check.getSnmpOID(), paramsForCheck);
         Inet ipDevice = checkDevice.getIp();
         String community = checkDevice.getSnmpCommunity();
-        String snmpOID = check.getSnmpOID();
 
         return CheckResult.builder()
                 .status(CheckResultStatus.OK)
                 .id(1L)
                 .result("IP: " + ipDevice.getAddress() + ", community: " + community + ", OID: " + snmpOID)
                 .build();
+    }
+
+    private String updateVariablesInOIDwithValues(String sourceOID, Map<String, String> params) {
+        String snmpOID = sourceOID;
+        for (String param : params.keySet()) {
+            snmpOID = snmpOID.replace("[#" + param + "]", params.get(param));
+        }
+        return snmpOID;
     }
 }
