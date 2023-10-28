33e02268319f1c7826978a8b21ef1062a36d9a55
Evgeny Koregin <evgkoregin@gmail.com>
2023-10-28 14:57:23 +0300

    Print type of params in editCustomer

--- src/main/java/com/ekoregin/nms/entity/TypeTechParameter.java
+++ src/main/java/com/ekoregin/nms/entity/TypeTechParameter.java
@@ -19,4 +19,9 @@ public class TypeTechParameter {
     private Long id;
     private String name;
     private String description;
+
+    @Override
+    public String toString() {
+        return name;
+    }
 }
