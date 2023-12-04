78fdb3285187aac4690bab708d4f808e9f639085
Evgeny Koregin <evgkoregin@gmail.com>
2023-12-04 22:32:10 +0300

    Add Jacoco plugin for tests Add application.properties for tests

--- src/test/java/com/ekoregin/nms/service/CustomerServiceImplTest.java
+++ src/test/java/com/ekoregin/nms/service/CustomerServiceImplTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -16,7 +16,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
 
-@ExtendWith(SpringExtension.class)
+@ExtendWith(MockitoExtension.class)
 class CustomerServiceImplTest {
 
     @Mock
